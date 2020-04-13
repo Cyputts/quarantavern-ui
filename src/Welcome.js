fed677843d46dd7f7fc8d2d8d5a5e1cda0b3a8dd
Michael Anderson <mike@omnicorn.lan>
2020-04-13 18:03:55 -0400

    first commit

---
+++ src/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Welcome.css';
+import bgimg from './bar.jpg';
+
+
+function Welcome() {
+  return(
+    <div class="bg">
+      <div class="welcome-container">
+	<h1 class="welcome-title">Quarantavern</h1>
+	<p class="welcome-body">I swear, this will be up very soon.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
